0447cb5e075cc0fb846c803d7b7336e988d0dc93
Yuriy <ybaturin@cloudlinux.com>
2019-01-09 21:44:30 +0500

    new type

--- template/tsc/{name_pc}/{name_pc}.tsx
+++ template/tsc/{name_pc}/{name_pc}.tsx
@@ -6,9 +6,9 @@ interface {{name_pc}}Props {
 }
 
 export const {{ name_pc }}: FunctionComponent<{{name_pc}}Props> = (props) => {
-  return (
-    <div className="woo-{{name_sc}}">
+    return (
+        <div className="woo-{{name_sc}}">
 
-    </div>
-  )
+        </div>
+    )
 };
