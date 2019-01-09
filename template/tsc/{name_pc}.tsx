8f8a926daf0633f483458a3f0953a173bf2e9f83
Yuriy <ybaturin@cloudlinux.com>
2019-01-09 21:26:31 +0500

    new type

--- template/tsc/{name_pc}.tsx
+++ template/tsc/{name_pc}.tsx
@@ -1,20 +1,14 @@
-import * as React from 'react';
+import React, { FunctionComponent } from 'react';
 import './{{name_pc}}.scss'
 
 interface {{name_pc}}Props {
 
 }
 
-export class {{name_pc}} extends Component<{{name_pc}}Props> {
-    render() {
-        return (
-            <div className="woo-{{name_sc}}">
+export const {{ name_pc }}: FunctionComponent<{{name_pc}}Props> = (props) => {
+  return (
+    <div className="woo-{{name_sc}}">
 
-            </div>
-        )
-    }
-}
-
-const {{ name_pc }}: React.StatelessComponent<{{name_pc}}Props> = (props) => {
-  return (<div>{{name_pc}}</div>)
-}
+    </div>
+  )
+};
